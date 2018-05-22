c511287681a7856ef993a39c92223e7af38ad69f
INTEGRATION\root <ancgate@yahoo.fr>
2018-05-21 23:31:35 -0400

    Add project files.

---
+++ Inspinia_MVC5_SeedProject/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security.Cookies;
+using phoebe.Models;
+
+[assembly: OwinStartupAttribute(typeof(phoebe.Startup))]
+namespace phoebe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
